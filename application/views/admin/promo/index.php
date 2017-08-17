784eb27af64c265f8a63649355fa6a14c2a1f5d9
fuckhriizal <hsevfakhri@gmail.com>
2017-08-17 12:41:48 +0700

    onchance promo

--- application/views/admin/promo/index.php
+++ application/views/admin/promo/index.php
@@ -1,6 +1,10 @@
 <div class="page-header">
     <h1>Data Promo Hotel</h1>
 </div>
+<a href="<?php echo base_url('/admin/promo/add'); ?>" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#telor">
+    Tambah Data
+</a>
+<br /><br />
 <table class="table table-hover table-responsive">
     <thead>
         <tr class="success">
@@ -23,8 +27,9 @@
                     <td><?php echo $promo->discount; ?></td>
                     <td><?php echo _toaktif('admin/promo/aktif/', $promo->idpromo, $promo->status); ?></td>
                     <td>
-                        <a href="<?php echo base_url('index.php/admin/promo/edit/' . $promo->idclass); ?>" class="btn btn-default btn-xs btn-primary" data-target="#telo" role="button" data-toggle="modal"><span class="glyphicon glyphicon-edit"></span> Edit</a>
+                        <a href="<?php echo base_url('index.php/admin/promo/edit/' . $promo->idclass); ?>" class="btn btn-default btn-xs btn-primary" data-target="#telo" role="button" data-toggle="modal"><span class="glyphicon glyphicon-edit"></span> Edit</a> <?php echo btn_delete('admin/promo/delete/' . $promo->idpromo); ?>
                     </td>
+                    <td></td>
                 </tr>
         <?php else: ?>
             <tr><td>Belum ada data !</td></tr> 
@@ -34,12 +39,12 @@
 
 <!-- Modal -->
 
-<div class="modal fade" id="telo" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<div class="modal fade" id="telor" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Event</h4>
+                <h4 class="modal-title">Events</h4>
             </div>
             <div class="modal-body">
                 <p>Loading...</p>
@@ -54,7 +59,7 @@
     </div>
 </div><!-- /.modal-dialog -->
 <script>
-    $('#telo').on('hidden.bs.modal', function() {
+    $('#telor').on('hidden.bs.modal', function() {
         $(this).removeData('bs.modal');
     });
 </script>
